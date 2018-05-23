356a153d491c695ae87aae2e18867596157553fe
karayuumac <y-kara0706@docomo.ne.jp>
2018-05-23 12:14:26 +0900

    add ABC044B

--- src/main/kotlin/score200/ABC044B.kt
+++ src/main/kotlin/score200/ABC044B.kt
@@ -5,5 +5,27 @@ package score200
  */
 fun main(args: Array<String>) {
     val string = readLine()!!
-    //val character = string.
+    /* 文字数を保存しておくリスト */
+    val list = mutableListOf<Int>()
+
+    /* aからzまでの文字数をカウントしてlistに格納 */
+    ('a'..'z').forEach { character ->
+        list.add(string.count { it == character })
+    }
+
+    if (list.isEven()) print("Yes") else print("No")
 }
+
+/**
+ * 入力された[MutableList<Int>の要素が全て偶数かどうかを返します。
+ *
+ * Return whether all elements of imputed [MutableList<Int>] are even.
+ */
+fun MutableList<Int>.isEven() : Boolean {
+    for (j in this) {
+        if (!j.isEven()) return false
+    }
+    return true
+}
+
+fun Int.isEven() : Boolean = this % 2 == 0
